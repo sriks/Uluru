970aba5312ca75962fafa06ee833c1ab41183330
Srikanth Sombhatla <srikanthsombhatla@gmail.com>
2019-08-23 14:50:29 +1000

    Initial Commit

---
+++ Uluru/Uluru.h
@@ -0,0 +1,19 @@
+//
+//  Uluru.h
+//  Uluru
+//
+//  Created by Srikanth Sombhatla on 23/8/19.
+//  Copyright © 2019 Tabcorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Uluru.
+FOUNDATION_EXPORT double UluruVersionNumber;
+
+//! Project version string for Uluru.
+FOUNDATION_EXPORT const unsigned char UluruVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Uluru/PublicHeader.h>
+
+
