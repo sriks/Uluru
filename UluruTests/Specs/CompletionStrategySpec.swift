f5ff53a8760fc6638352a3843085a6fd65f135d0
Srikanth Sombhatla <srikanth.sombhatla@tabcorp.com.au>
2019-09-12 10:53:37 +1000

    WIP: Added additional tests on retry strategy.

--- UluruTests/Specs/CompletionStrategySpec.swift
+++ UluruTests/Specs/CompletionStrategySpec.swift
@@ -8,6 +8,7 @@ import Nimble
 class CompletionStrategySpec: QuickSpec {
     override func spec() {
         TestHelper.markWaitExpecationAsAPIRequest()
+        
         context("When completion strategy is .goahead") {
             var service: ServiceProvider<PostmanEcho>!
 
@@ -48,7 +49,7 @@ class CompletionStrategySpec: QuickSpec {
                 expect(model).notTo( beNil() )
             }
 
-            fit("should invoke plugins exptected times") {
+            it("should invoke plugins on every retry") {
                 let completionProvider = MockRetryCompletionStrategyProvider(maxRetries: 2)
                 // +1 because the retries are applied after the first attempt.
                 let expectedInvocations = completionProvider.maxRetries + 1
@@ -71,8 +72,18 @@ class CompletionStrategySpec: QuickSpec {
                 expect(aPlugin.invocationCount.mutateResult).to( equal(expectedInvocations) )
             }
 
-            it("should invoke completion handler only once") {
-                
+            it("should invoke completion handler only once irrespective of retries") {
+                let completionProvider = MockRetryCompletionStrategyProvider(maxRetries: 2)
+                service = ServiceProvider(completionStrategy: completionProvider)
+                var actualCompletionInvocations = 0
+                waitUntil { done in
+                    let _ = service.request(.justGet, expecting: EmptyDecodableModel.self, completion: { (result) in
+                        actualCompletionInvocations += 1
+                        done()
+                    })
+                }
+
+                expect(actualCompletionInvocations).to( equal(1) )
             }
 
         }
