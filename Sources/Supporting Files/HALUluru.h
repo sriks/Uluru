61a4c0358f312b3b02432259d0f77265bdab8cb5
Lun, Yunzhi <Yunzhi.Lun@tabcorp.com.au>
2019-11-15 14:04:55 +1100

    [CUCH-3090] Expose required .h files as public

--- Sources/Supporting Files/HALUluru.h
+++ Sources/Supporting Files/HALUluru.h
@@ -7,7 +7,3 @@ FOUNDATION_EXPORT double HALUluruVersionNumber;
 
 //! Project version string for HALUluru.
 FOUNDATION_EXPORT const unsigned char HALUluruVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <HALUluru/PublicHeader.h>
-
-
