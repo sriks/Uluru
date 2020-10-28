dba34dcdb8b9325f19a40760538687f80578bcab
Yoo-Jin, Lee <Lee.YooJin@tabcorp.com.au>
2020-10-28 15:43:56 +1100

    [CUCH-12577] Add No Network Connection Property (#14)

---
+++ Sources/Uluru/Service/ServiceError+Extension.swift
@@ -0,0 +1,61 @@
+//  Copyright © 2020 Tabcorp. All rights reserved.
+
+/**
+
+ Convenience  property for a network connection error
+
+ */
+extension NSError {
+
+    private enum Constant {
+        static let networkErrors: [URLError.Code] = [.networkConnectionLost,
+                                                     .notConnectedToInternet]
+    }
+
+    /**
+
+     Convenience property to indicate if there is an internet connection error or not
+
+     */
+    public var isNetworkConnectionError: Bool {
+        let urlErrorCode = URLError.Code(rawValue: code)
+        return domain == NSURLErrorDomain && Constant.networkErrors.contains(urlErrorCode)
+    }
+}
+
+/**
+
+ Convenience functions to get underlying errors
+
+ */
+extension ServiceError {
+
+    var nsError: NSError? {
+        guard case let .underlying(underlyingError, _) = self else {
+            return nil
+        }
+        return underlyingError as NSError
+    }
+
+    /**
+
+     Returns an underlying URLError.Code if available
+
+     */
+    var urlErrorCode: URLError.Code? {
+        guard let error = nsError else {
+            return nil
+        }
+        return URLError.Code(rawValue: error.code)
+    }
+
+    /**
+
+     Convenience property to indicate if there is an internet connection error or not
+
+     */
+    public var isNetworkConnectionError: Bool {
+        return nsError?.isNetworkConnectionError == true
+    }
+
+}
